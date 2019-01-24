0a5bb413cecf51636d91a4d497520478e5ea3ff6
fasosnql <nowak_przemek@yahoo.com>
2019-01-24 10:56:05 +0100

    feat: lines and areas example

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ const htmlEl = document.querySelector('#content');
 const navEl = document.querySelector('#nav');
 
 
+const currentID = window.location.hash.replace('#', '');
+
+if (currentID) {
+  htmlEl.innerHTML = examples[currentID].template;
+}
+
 navEl.addEventListener('click', (e) => {
-  if (e.target.nodeName === 'LI') {
+  if (e.target.nodeName === 'A') {
     htmlEl.innerHTML = examples[e.target.id].template;
   }
 });
